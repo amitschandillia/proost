7993406f54e7ca3aca5a2c2bda61a82cd12b863e
Amit Schandillia <amdroid666@gmail.com>
2019-02-14 12:22:44 +0000

    Implemented Jest with dummy test files

--- graph/dummy.js
+++ graph/dummy.js
@@ -1,3 +1,10 @@
 'use strict';
 
 export const hello = () => 'Hello';
+
+export const add = (x, y) => {
+  if(typeof x == 'string' || typeof y == 'string'){
+    return null;
+  }
+  return x + y;
+};
--- graph/tests/dummy.test.js
+++ graph/tests/dummy.test.js
@@ -9,10 +9,19 @@
 
 
 
-import { hello } from '../dummy';
+import { hello, add } from '../dummy';
 
 describe('hello', () => {
   it('Should output hello', () => {
     expect(hello()).toBe('Hello');
+  });
+});
+
+describe('add', () => {
+  it('Should add two numbers', () => {
+    expect(add(1, 2)).toBe(3);
+  });
+  it('Should not add strings', () => {
+    expect(add(1, '2')).toBe(null);
   });
 });
