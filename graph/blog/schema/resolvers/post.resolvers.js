f6fccbbfd52967c84ced967bed89d468ef575d6c
Amit Schandillia <amdroid666@gmail.com>
2019-02-24 12:21:50 +0000

    Linted post resolvers

--- graph/blog/schema/resolvers/post.resolvers.js
+++ graph/blog/schema/resolvers/post.resolvers.js
@@ -1,41 +1,30 @@
-'use strict';
-
 // Imports: Mongoose
 import mongoose from 'mongoose';
+// Imports: graphql-fields
+import graphqlFields from 'graphql-fields';
 // Imports: Models
 import Post from '../../models/post';
 import Author from '../../models/author';
-// Imports: graphql-fields
-import graphqlFields from 'graphql-fields';
 
 // GraphQL: Resolvers
 module.exports = {
   // Resolve queries
   Query: {
     // Retrieve all posts
-    posts: () => {
-      return Post
+    posts: () => Post
       .find()
-      .then(posts => {
-        return posts.map(post => {
-          return { ...post._doc };
-        });
-      })
-      .catch(err => {
+      .then(posts => posts.map(post => post._doc))
+      .catch((err) => {
         throw err;
-      });
-    },
+      }),
     // Retrieve post by ID
-    post: (root, args, context) => {
-      return Post
-        .findById(args._id)
-        .then(post => {
-          return post._doc;
-        })
-        .catch(err => {
-          throw err;
-        });
-    },
+    post: (root, args) => Post
+      .findById(args._id)
+      .then(post => post._doc)
+      .catch((err) => {
+        throw err;
+      }),
+    // },
   },
 
   // Resolve author of given post
@@ -46,42 +35,43 @@ module.exports = {
       // Retrieve fields returned by parent, if any
       const fieldsInParent = Object.keys(parent.author._doc);
       // Check if queried fields already exist in parent
-      const available = queriedFields.every((field) => fieldsInParent.includes(field));
-      if(parent.author && available) {
+      const available = queriedFields.every(field => fieldsInParent.includes(field));
+      let authorData = {};
+      if (parent.author && available) {
         // If parent data is available and includes queried fields, no need to query db
-        return parent.author;
-      }
-      else {
+        authorData = parent.author;
+        // return parent.author;
+      } else {
         // Otherwise, query db and retrieve data
-        return Author.findOne({'posts._id': parent._id}, (err, docs) => {
-          if (docs){ return docs; }
-          if (err){ throw err; }
+        authorData = Author.findOne({ 'posts._id': parent._id }, (err, docs) => {
+          if (err) { throw err; }
+          return docs;
         });
       }
+      return authorData;
     },
   },
 
   // Resolve mutations
   Mutation: {
     // Create a new post
-    createPost: async (root, args, context) => {
+    createPost: async (root, args) => {
       const session = await mongoose.startSession();
       session.startTransaction();
       try {
         // Create session object
         const opts = { session, new: true };
         // Operation 1: Insert into posts collection
-        const isPublished = args.postInput.isPublished;
+        const { isPublished } = args.postInput;
         let milestones = {};
         const currTime = new Date();
-        if(isPublished){
+        if (isPublished) {
           milestones = {
             createdAt: currTime,
             updatedAt: currTime,
-            publishedAt: currTime
+            publishedAt: currTime,
           };
-        }
-        else {
+        } else {
           milestones = {
             createdAt: currTime,
             updatedAt: currTime,
@@ -95,20 +85,20 @@ module.exports = {
           milestones,
         }).save(opts);
         // Throw error and abort transaction if operation fails, i.e. createdPost = null
-        if(!createdPost) throw new Error('Couldn\'t create post');
+        if (!createdPost) throw new Error('Couldn\'t create post');
         // Work object to be used in the next operation
         const work = {
           posts: {
             _id: createdPost._id,
             isPublished: args.postInput.isPublished,
             title: args.postInput.title,
-          }
+          },
         };
         // Operation 2: Update authors collection
-        const updatedAuthor = await Author.
-        findOneAndUpdate({_id: args.postInput.author._id}, {$push: work}, opts);
+        const updatedAuthor = await Author
+          .findOneAndUpdate({ _id: args.postInput.author._id }, { $push: work }, opts);
         // Throw error and abort transaction if operation fails, i.e. updatedAuthor = null
-        if(!updatedAuthor) throw new Error('Couldn\'t update author');
+        if (!updatedAuthor) throw new Error('Couldn\'t update author');
         await session.commitTransaction();
         session.endSession();
         // Return post data as GraphQL response
@@ -122,5 +112,5 @@ module.exports = {
     },
     // Update an existing post
     // updatePost:
-  }
+  },
 };
