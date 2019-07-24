2bfe0f66d6f08d6e6c2909272db2f50ebc5e4852
Amit Schandillia <amdroid666@gmail.com>
2019-07-24 04:02:56 +0000

    Authentication with Google, tested stable.

---
+++ web-stable-auth/routes/auth-routes.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+
+import express from 'express';
+import passport from 'passport';
+import session from 'express-session';
+import passportSetup from '../config/passport-setup';
+
+const router = express.Router();
+
+router.use(session({ resave: true, secret: '123456', saveUninitialized: true }));
+
+// auth login
+router.get('/login', (req, res) => {
+  res.send('login...');
+});
+// auth logout
+router.get('/logout', (req, res) => {
+  res.send('log out...');
+});
+
+router.get('/google', (req, res, next) => {
+  req.session.callback = req.query.callback;
+  next();
+}, passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  console.log('FROM REDIRECT: ', req.user);
+  res.redirect(req.session.callback);
+});
+
+module.exports = router;
